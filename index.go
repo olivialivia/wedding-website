897bd31835bcf7756202af208c351fa09d9b1935
olivialivia <94136530+olivialivia@users.noreply.github.com>
2022-01-24 20:00:35 +0000

    Add JS, http req, more css

--- index.go
+++ index.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/submit", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Access-Control-Allow-Origin", "*")
+		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			rw.WriteHeader(200)
+			return
+		}
+		rw.WriteHeader(200)
+		if err := r.ParseForm(); err != nil {
+			rw.WriteHeader(500)
+			return
+		}
+		fmt.Printf("received form %+v \n", r.PostForm)
+	})
+	fmt.Println("listening")
+	if err := http.ListenAndServe("127.0.0.1:5501", nil); err != nil {
+		panic(err)
+	}
+}
