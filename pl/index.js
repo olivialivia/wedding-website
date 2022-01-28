7f3df68cc10fd2f0086f12a988910c3829c21975
olivialivia <94136530+olivialivia@users.noreply.github.com>
2022-01-28 13:30:00 +0000

    Add pl version

---
+++ pl/index.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", function () {
+    let form = document.getElementById("rsvp-form");
+    let log = document.getElementById("submitted-form");
+    let btn1 = document.getElementById("btn-confirm");
+
+    btn1.addEventListener("click", async (event) => {
+        log.textContent = "Thank you for submitting the form!";
+        event.preventDefault();
+        const formData = new FormData(form);
+
+        let success = false;
+
+        try {
+            let response = await fetch("http://127.0.0.1:5501/submit", {
+                method: "POST",
+                body: formData,
+            });
+            success = response.ok;
+        } catch (err) {
+            console.log("request errpr", err);
+        }
+
+        if (!success) {
+            log.textContent = "There was a problem submitting the form. Please retry or email us.";
+        }
+    });
+});
